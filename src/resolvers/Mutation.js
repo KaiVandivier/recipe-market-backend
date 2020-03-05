dab29b2b95ec06a03fe9d787c93f004084247b92
Kai Vandivier <kpvandivier@gmail.com>
2020-03-04 21:13:52 -0800

    Add edit and delete item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -20,6 +20,24 @@ const Mutation = {
     }, info)
     console.log(res);
     return res
+  },
+  async editItem(_, args, ctx, info) {
+    // TODO: Check for permissions?
+    const updates = { ...args };
+    delete updates.id
+
+    const res = await ctx.db.mutation.updateItem({
+      data: updates,
+      where: { id: args.id }
+    }, info)
+    console.log(res);
+    return res;
+  },
+  async deleteItem(_, { id }, ctx, info) {
+    const res = await ctx.db.mutation.deleteItem({
+      where: { id }
+    }, info);
+    return res;
   }
 };
 
