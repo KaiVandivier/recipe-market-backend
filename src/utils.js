9957d875343ed594586cf12591e3e320b695aafb
Kai Vandivier <kpvandivier@gmail.com>
2020-03-11 11:06:50 -0700

    Modify name and text

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
-const hasPermissions = function hasPermissions(user, permissionsNeeded) {
+const checkPermissions = function checkPermissions(user, permissionsNeeded) {
   const permissionsMatched = user.permissions.some(permission =>
     permissionsNeeded.includes(permission)
   );
   if (!permissionsMatched)
-    throw new Error(`You do not have sufficient permissions to do this action. 
-      You need: ${permissionsNeeded};
+    throw new Error(`You do not have sufficient permissions to do this action.
+      You need at least one of: ${permissionsNeeded};
       You have: ${user.permissions}`
     );
   return permissionsMatched;
 };
 
-module.exports = { hasPermissions };
+module.exports = { checkPermissions };
